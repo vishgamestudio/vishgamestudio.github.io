70c130f821e94c72cf4439674650424e3d31c213
Diemorth <biemorth@gmail.com>
2024-11-06 17:20:15 -0300

    Privacy page

--- js/button-setup.js
+++ js/button-setup.js
@@ -7,93 +7,141 @@ var functionforscroll = function(id){
 document.addEventListener("DOMContentLoaded", function(){
 
   var element = document.getElementsByClassName("HomeExport");
-  element[0].setAttribute("id", "BannerTab");
+  if (element[0])
+    element[0].setAttribute("id", "BannerTab");
   var element = document.getElementsByClassName("DivJustKing");
-  element[0].setAttribute("id", "GamesTab");
+  if (element[0])
+    element[0].setAttribute("id", "GamesTab");
   var element = document.getElementsByClassName("DivVish");
-  element[0].setAttribute("id", "SobreTab");
+  if (element[0])
+    element[0].setAttribute("id", "SobreTab");
   var element = document.getElementsByClassName("TTuloVish");
-  element[0].setAttribute("id", "SobreTabTarget");
+  if (element[0])
+    element[0].setAttribute("id", "SobreTabTarget");
   var element = document.getElementsByClassName("DivContato");
-  element[0].setAttribute("id", "ContactTab");
+  if (element[0])
+    element[0].setAttribute("id", "ContactTab");
   var element = document.getElementsByClassName("DivPresskit");
-  element[0].setAttribute("id", "PresskitTab");
+  if (element[0])
+    element[0].setAttribute("id", "PresskitTab");
   var element = document.getElementsByClassName("DivJobs");
-  element[0].setAttribute("id", "JobsTab");
+  if (element[0])
+    element[0].setAttribute("id", "JobsTab");
 
   var element = document.getElementsByClassName("Inicio");
-  element[0].classList.add("NavButton");
-  element[0].addEventListener('click', function() { functionforscroll("BannerTab")}, false);
+  if (element[0])
+    element[0].classList.add("NavButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { functionforscroll("BannerTab")}, false);
 
   var element = document.getElementsByClassName("Games");
-  element[0].classList.add("NavButton");
-  element[0].addEventListener('click', function() { functionforscroll("GamesTab")}, false);
+  if (element[0])
+    element[0].classList.add("NavButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { functionforscroll("GamesTab")}, false);
 
   var element = document.getElementsByClassName("Sobre");
-  element[0].classList.add("NavButton");
-  element[0].addEventListener('click', function() { functionforscroll("SobreTabTarget")}, false);
+  if (element[0])
+    element[0].classList.add("NavButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { functionforscroll("SobreTabTarget")}, false);
 
   var element = document.getElementsByClassName("Contato");
-  element[0].classList.add("NavButton");
-  element[0].addEventListener('click', function() { functionforscroll("ContactTab")}, false);
+  if (element[0])
+    element[0].classList.add("NavButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { functionforscroll("ContactTab")}, false);
 
   var element = document.getElementsByClassName("Presskit");
-  element[0].classList.add("NavButton");
-  element[0].addEventListener('click', function() { functionforscroll("PresskitTab")}, false);
+  if (element[0])
+    element[0].classList.add("NavButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { functionforscroll("PresskitTab")}, false);
 
   var element = document.getElementsByClassName("Jobs");
-  element[0].classList.add("NavButton");
-  element[0].addEventListener('click', function() { functionforscroll("JobsTab")}, false);
+  if (element[0])
+    element[0].classList.add("NavButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { functionforscroll("JobsTab")}, false);
 
   var element = document.getElementsByClassName("Language");
-  element[0].classList.add("NavButton");
+  if (element[0])
+    element[0].classList.add("NavButton");
 
   var element = document.getElementsByClassName("Justkingsteamicon");
-  element[0].classList.add("RoundButton");
-  element[0].addEventListener('click', function() { window.open('https://store.steampowered.com/app/1059980/Just_King/', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RoundButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://store.steampowered.com/app/1059980/Just_King/', '_blank'); }, false);
 
   var element = document.getElementsByClassName("Justkingyoutubeicon");
-  element[0].classList.add("RoundButton");
-  element[0].addEventListener('click', function() { window.open('https://www.youtube.com/@vishgamestudio9934/videos', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RoundButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://www.youtube.com/@vishgamestudio9934/videos', '_blank'); }, false);
 
   var element = document.getElementsByClassName("Discordicon");
-  element[0].classList.add("RoundButton");
-  element[0].addEventListener('click', function() { window.open('https://discord.gg/eChV9gS5jS', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RoundButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://discord.gg/eChV9gS5jS', '_blank'); }, false);
   var element = document.getElementsByClassName("EMailicon");
-  element[0].classList.add("RoundButton");
-  element[0].addEventListener('click', function() { window.open('mailto:contact@vishgamestudio.com', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RoundButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('mailto:contact@vishgamestudio.com', '_blank'); }, false);
   var element = document.getElementsByClassName("Steamicon");
-  element[0].classList.add("RoundButton");
-  element[0].addEventListener('click', function() { window.open('https://steamcommunity.com/app/1059980/discussions/', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RoundButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://steamcommunity.com/app/1059980/discussions/', '_blank'); }, false);
 
   var element = document.getElementsByClassName("Frame9");
-  element[0].classList.add("RoundButton");
-  element[0].addEventListener('click', function() { window.open('https://drive.google.com/drive/folders/1ITk78Qt1wGptGhW3WD9xECLgr0HBk9TZ?usp=sharing', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RoundButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://drive.google.com/drive/folders/1ITk78Qt1wGptGhW3WD9xECLgr0HBk9TZ?usp=sharing', '_blank'); }, false);
   var element = document.getElementsByClassName("Frame10");
-  element[0].classList.add("RoundButton");
-  element[0].addEventListener('click', function() { window.open('https://drive.google.com/drive/u/4/folders/1ZrAmnBklwCDwJrziyTh5SKPKq-7LJ24u', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RoundButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://drive.google.com/drive/u/4/folders/1ZrAmnBklwCDwJrziyTh5SKPKq-7LJ24u', '_blank'); }, false);
   var element = document.getElementsByClassName("Frame24");
-  element[0].classList.add("RectButton");
-  element[0].addEventListener('click', function() { window.open('https://store.steampowered.com/app/1059980/Just_King/', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RectButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://store.steampowered.com/app/1059980/Just_King/', '_blank'); }, false);
   var element = document.getElementsByClassName("Frame11");
-  element[0].classList.add("RectButton");
-  element[0].addEventListener('click', function() { window.open('https://drive.google.com/drive/folders/1ITk78Qt1wGptGhW3WD9xECLgr0HBk9TZ?usp=sharing', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RectButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://drive.google.com/drive/folders/1ITk78Qt1wGptGhW3WD9xECLgr0HBk9TZ?usp=sharing', '_blank'); }, false);
   var element = document.getElementsByClassName("Frame12");
-  element[0].classList.add("RectButton");
-  element[0].addEventListener('click', function() { window.open('https://drive.google.com/drive/u/4/folders/1ZrAmnBklwCDwJrziyTh5SKPKq-7LJ24u', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RectButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://drive.google.com/drive/u/4/folders/1ZrAmnBklwCDwJrziyTh5SKPKq-7LJ24u', '_blank'); }, false);
 
   var element = document.getElementsByClassName("Formbutton");
-  element[0].classList.add("RectButton");
-  element[0].addEventListener('click', function() { window.open('https://docs.google.com/forms/d/e/1FAIpQLSf8hHLqhoairhEj0o4sH05gDKuIAqUR8Hr8T7IxbxsirYMg8A/viewform', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RectButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://docs.google.com/forms/d/e/1FAIpQLSf8hHLqhoairhEj0o4sH05gDKuIAqUR8Hr8T7IxbxsirYMg8A/viewform', '_blank'); }, false);
  
   var element = document.getElementsByClassName("VideoFrame");
-  element[0].classList.add("RectButton");
-  element[0].addEventListener('click', function() { window.open('https://store.steampowered.com/app/1059980/Just_King/', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RectButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://store.steampowered.com/app/1059980/Just_King/', '_blank'); }, false);
   
   var element = document.getElementsByClassName("Frame22");
-  element[0].classList.add("RectButton");
-  element[0].addEventListener('click', function() { window.open('https://www.linkedin.com/in/enriquewr/', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RectButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://www.linkedin.com/in/enriquewr/', '_blank'); }, false);
   var element = document.getElementsByClassName("Frame19");
-  element[0].classList.add("RectButton");
-  element[0].addEventListener('click', function() { window.open('https://www.linkedin.com/in/diemorth/', '_blank'); }, false);
+  if (element[0])
+    element[0].classList.add("RectButton");
+  if (element[0])
+    element[0].addEventListener('click', function() { window.open('https://www.linkedin.com/in/diemorth/', '_blank'); }, false);
 }, false);
--- js/translation.js
+++ js/translation.js
@@ -4,7 +4,8 @@ var languageIsDefault = false;
 document.addEventListener("DOMContentLoaded", function(){
 
   var element = document.getElementsByClassName("Language");
-  element[0].addEventListener('click', function() { toggleLanguage(); }, false);
+  if (element[0])
+    element[0].addEventListener('click', function() { toggleLanguage(); }, false);
   
   toggleLanguage();
 }, false);
@@ -13,125 +14,186 @@ var toggleLanguage = function(id){
   languageIsDefault = !languageIsDefault;
   if (languageIsDefault){
     var element = document.getElementsByClassName("LanguageIndicator");
-    element[0].innerHTML = "EN"
+    if (element[0])
+      element[0].innerHTML = "EN"
     var element = document.getElementsByClassName("Inicio");
-    element[0].innerHTML = "START";
+    if (element[0])
+      element[0].innerHTML = "START";
     var element = document.getElementsByClassName("Games");
-    element[0].innerHTML = "GAMES";
+    if (element[0])
+      element[0].innerHTML = "GAMES";
     var element = document.getElementsByClassName("Sobre");
-    element[0].innerHTML = "ABOUT";
+    if (element[0])
+      element[0].innerHTML = "ABOUT";
     var element = document.getElementsByClassName("Contato");
-    element[0].innerHTML = "CONTACT";
+    if (element[0])
+      element[0].innerHTML = "CONTACT";
     var element = document.getElementsByClassName("Presskit");
-    element[0].innerHTML = "PRESS KIT";
+    if (element[0])
+      element[0].innerHTML = "PRESS KIT";
     var element = document.getElementsByClassName("Jobs");
-    element[0].innerHTML = "JOBS";
+    if (element[0])
+      element[0].innerHTML = "JOBS";
     var element = document.getElementsByClassName("Aboutsectiontitle");
-    element[0].innerHTML = "VISH";
+    if (element[0])
+      element[0].innerHTML = "VISH";
     var element = document.getElementsByClassName("Abouttexttitle");
-    element[0].innerHTML = "About our studio";
+    if (element[0])
+      element[0].innerHTML = "About our studio";
     var element = document.getElementsByClassName("Abouttext1");
-    element[0].innerHTML = "We are a Brazilian company based in Bahia, founded in 2022. We develop games for Steam and are planning to expand in the near future.";
+    if (element[0])
+      element[0].innerHTML = "We are a Brazilian company based in Bahia, founded in 2022. We develop games for Steam and are planning to expand in the near future.";
     var element = document.getElementsByClassName("Abouttext2");
-    element[0].innerHTML = "Our primary focus is to create engaging, entertaining games with distinctive designs. Currently, we collaborate with a publisher to distribute our games in Asia, while we handle self-publishing for the rest of the world.";
+    if (element[0])
+      element[0].innerHTML = "Our primary focus is to create engaging, entertaining games with distinctive designs. Currently, we collaborate with a publisher to distribute our games in Asia, while we handle self-publishing for the rest of the world.";
     var element = document.getElementsByClassName("CoFundadorGameDesigner");
-    element[0].innerHTML = "Co-founder, Game designer";
-    element[1].innerHTML = "Co-founder, Game designer";
+    if (element[0])
+      element[0].innerHTML = "Co-founder, Game designer";
+    if (element[1])
+      element[1].innerHTML = "Co-founder, Game designer";
     var element = document.getElementsByClassName("Contactsectiontitle");
-    element[0].innerHTML = "Did you like it? Contact us!";
+    if (element[0])
+      element[0].innerHTML = "Did you like it? Contact us!";
     var element = document.getElementsByClassName("Contacttext");
-    element[0].innerHTML = "We are available for contact through our platforms.";
+    if (element[0])
+      element[0].innerHTML = "We are available for contact through our platforms.";
     var element = document.getElementsByClassName("Discordtext");
-    element[0].innerHTML = "Our community discord.";
+    if (element[0])
+      element[0].innerHTML = "Our community discord.";
     var element = document.getElementsByClassName("Emailtext");
-    element[0].innerHTML = "Send us an email!";
+    if (element[0])
+      element[0].innerHTML = "Send us an email!";
     var element = document.getElementsByClassName("Steamtext");
-    element[0].innerHTML = "Our Steam forum.";
+    if (element[0])
+      element[0].innerHTML = "Our Steam forum.";
     var element = document.getElementsByClassName("Presskittext");
-    element[0].innerHTML = "Arts and information for the press, or for content creators! We will always be updating these materials.";
+    if (element[0])
+      element[0].innerHTML = "Arts and information for the press, or for content creators! We will always be updating these materials.";
     var element = document.getElementsByClassName("Presskittextvish");
-    element[0].innerHTML = "Vish press kit";
+    if (element[0])
+      element[0].innerHTML = "Vish press kit";
     var element = document.getElementsByClassName("Presskitsectiontitle");
-    element[0].innerHTML = "Press kit";
+    if (element[0])
+      element[0].innerHTML = "Press kit";
     var element = document.getElementsByClassName("Presskittextking");
-    element[0].innerHTML = "Just King press kit";
+    if (element[0])
+      element[0].innerHTML = "Just King press kit";
     var element = document.getElementsByClassName("Baixar");
-    element[0].innerHTML = "Download!";
-    element[1].innerHTML = "Download!";
+    if (element[0])
+      element[0].innerHTML = "Download!";
+    if (element[1])
+      element[1].innerHTML = "Download!";
     var element = document.getElementsByClassName("Jobssectiontitle");
-    element[0].innerHTML = "Jobs";
+    if (element[0])
+      element[0].innerHTML = "Jobs";
     var element = document.getElementsByClassName("Jobstext");
-    element[0].innerHTML = "If you are interested in working with us, please fill out the form below!";
+    if (element[0])
+      element[0].innerHTML = "If you are interested in working with us, please fill out the form below!";
     var element = document.getElementsByClassName("PreencherFormulRio");
-    element[0].innerHTML = "FILL OUT THE FORM";
+    if (element[0])
+      element[0].innerHTML = "FILL OUT THE FORM";
     var element = document.getElementsByClassName("Gamestexttitle");
-    element[0].innerHTML = "The most adorable horde mower in a roguelite package!";
+    if (element[0])
+      element[0].innerHTML = "The most adorable horde mower in a roguelite package!";
     var element = document.getElementsByClassName("Gamestext1");
-    element[0].innerHTML = "Just King has sold over 120,000 copies on Steam, marking our debut into commercial game publishing.";
+    if (element[0])
+      element[0].innerHTML = "Just King has sold over 120,000 copies on Steam, marking our debut into commercial game publishing.";
     var element = document.getElementsByClassName("Gamestext2");
-    element[0].innerHTML = "In this game, you will assemble your party of heroes to protect the king as you venture through fantastical lands, battling fearsome kings and deadly armies. Strategically use your treasure to recruit and empower powerful heroes... or the Bard.";
+    if (element[0])
+      element[0].innerHTML = "In this game, you will assemble your party of heroes to protect the king as you venture through fantastical lands, battling fearsome kings and deadly armies. Strategically use your treasure to recruit and empower powerful heroes... or the Bard.";
     var element = document.getElementsByClassName("VejaOJogoNaSteam");
-    element[0].innerHTML = "Check out on Steam!";
+    if (element[0])
+      element[0].innerHTML = "Check out on Steam!";
   }else{
     var element = document.getElementsByClassName("LanguageIndicator");
-    element[0].innerHTML = "PT"
+    if (element[0])
+      element[0].innerHTML = "PT"
     var element = document.getElementsByClassName("Inicio");
-    element[0].innerHTML = "INÍCIO";
+    if (element[0])
+      element[0].innerHTML = "INÍCIO";
     var element = document.getElementsByClassName("Games");
-    element[0].innerHTML = "JOGOS";
+    if (element[0])
+      element[0].innerHTML = "JOGOS";
     var element = document.getElementsByClassName("Sobre");
-    element[0].innerHTML = "SOBRE";
+    if (element[0])
+      element[0].innerHTML = "SOBRE";
     var element = document.getElementsByClassName("Contato");
-    element[0].innerHTML = "CONTATO";
+    if (element[0])
+      element[0].innerHTML = "CONTATO";
     var element = document.getElementsByClassName("Presskit");
-    element[0].innerHTML = "PRESS KIT";
+    if (element[0])
+      element[0].innerHTML = "PRESS KIT";
     var element = document.getElementsByClassName("Jobs");
-    element[0].innerHTML = "VAGAS";
+    if (element[0])
+      element[0].innerHTML = "VAGAS";
     var element = document.getElementsByClassName("Aboutsectiontitle");
-    element[0].innerHTML = "VISH";
+    if (element[0])
+      element[0].innerHTML = "VISH";
     var element = document.getElementsByClassName("Abouttexttitle");
-    element[0].innerHTML = "Sobre nosso estúdio";
+    if (element[0])
+      element[0].innerHTML = "Sobre nosso estúdio";
     var element = document.getElementsByClassName("Abouttext1");
-    element[0].innerHTML = "Somos uma empresa brasileira localizada na Bahia, fundada em 2022, produzimos jogos para Steam e estamos planejando expandir em breve.";
+    if (element[0])
+      element[0].innerHTML = "Somos uma empresa brasileira localizada na Bahia, fundada em 2022, produzimos jogos para Steam e estamos planejando expandir em breve.";
     var element = document.getElementsByClassName("Abouttext2");
-    element[0].innerHTML = "Nosso foco é fazer jogos divertidos, interessantes e com design marcante. Atualmente trabalhamos com uma publisher que faz a distribuição dos nossos jogos na Ásia e fazemos self publishing no resto do mundo.";
+    if (element[0])
+      element[0].innerHTML = "Nosso foco é fazer jogos divertidos, interessantes e com design marcante. Atualmente trabalhamos com uma publisher que faz a distribuição dos nossos jogos na Ásia e fazemos self publishing no resto do mundo.";
     var element = document.getElementsByClassName("CoFundadorGameDesigner");
-    element[0].innerHTML = "Co-fundador, Game designer";
+    if (element[0])
+      element[0].innerHTML = "Co-fundador, Game designer";
     element[1].innerHTML = "Co-fundador, Game designer";
     var element = document.getElementsByClassName("Contactsectiontitle");
-    element[0].innerHTML = "Gostou? Contate-nos!";
+    if (element[0])
+      element[0].innerHTML = "Gostou? Contate-nos!";
     var element = document.getElementsByClassName("Contacttext");
-    element[0].innerHTML = "Estamos disponíveis para contato através das nossas plataformas.";
+    if (element[0])
+      element[0].innerHTML = "Estamos disponíveis para contato através das nossas plataformas.";
     var element = document.getElementsByClassName("Discordtext");
-    element[0].innerHTML = "O discord da nossa comunidade.";
+    if (element[0])
+      element[0].innerHTML = "O discord da nossa comunidade.";
     var element = document.getElementsByClassName("Emailtext");
-    element[0].innerHTML = "Nos envie um e-mail!";
+    if (element[0])
+      element[0].innerHTML = "Nos envie um e-mail!";
     var element = document.getElementsByClassName("Steamtext");
-    element[0].innerHTML = "Nosso fórum na Steam.";
+    if (element[0])
+      element[0].innerHTML = "Nosso fórum na Steam.";
     var element = document.getElementsByClassName("Presskittext");
-    element[0].innerHTML = "Artes e infos para a imprensa, ou para criadores de conteúdo! Estaremos sempre atualizando esses materiais.";
+    if (element[0])
+      element[0].innerHTML = "Artes e infos para a imprensa, ou para criadores de conteúdo! Estaremos sempre atualizando esses materiais.";
     var element = document.getElementsByClassName("Presskittextvish");
-    element[0].innerHTML = "Press kit da Vish";
+    if (element[0])
+      element[0].innerHTML = "Press kit da Vish";
     var element = document.getElementsByClassName("Presskitsectiontitle");
-    element[0].innerHTML = "Press kit";
+    if (element[0])
+      element[0].innerHTML = "Press kit";
     var element = document.getElementsByClassName("Presskittextking");
-    element[0].innerHTML = "Press kit do Just King";
+    if (element[0])
+      element[0].innerHTML = "Press kit do Just King";
     var element = document.getElementsByClassName("Baixar");
-    element[0].innerHTML = "Download!";
-    element[1].innerHTML = "Download!";
+    if (element[0])
+      element[0].innerHTML = "Download!";
+    if (element[1])
+      element[1].innerHTML = "Download!";
     var element = document.getElementsByClassName("Jobssectiontitle");
-    element[0].innerHTML = "Vagas";
+    if (element[0])
+      element[0].innerHTML = "Vagas";
     var element = document.getElementsByClassName("Jobstext");
-    element[0].innerHTML = "Se você se interessou em trabalhar conosco, preencha o formulário abaixo!";
+    if (element[0])
+      element[0].innerHTML = "Se você se interessou em trabalhar conosco, preencha o formulário abaixo!";
     var element = document.getElementsByClassName("PreencherFormulRio");
-    element[0].innerHTML = "PREENCHER FORMULÁRIO";
+    if (element[0])
+      element[0].innerHTML = "PREENCHER FORMULÁRIO";
     var element = document.getElementsByClassName("Gamestexttitle");
-    element[0].innerHTML = "O moedor de hordas mais fofo que existe no formato roguelite!";
+    if (element[0])
+      element[0].innerHTML = "O moedor de hordas mais fofo que existe no formato roguelite!";
     var element = document.getElementsByClassName("Gamestext1");
-    element[0].innerHTML = "Just King conta com mais de 120 mil cópias vendidas na Steam, é o nosso primeiro jogo publicado comercialmente.";
+    if (element[0])
+      element[0].innerHTML = "Just King conta com mais de 120 mil cópias vendidas na Steam, é o nosso primeiro jogo publicado comercialmente.";
     var element = document.getElementsByClassName("Gamestext2");
-    element[0].innerHTML = "Nesse jogo você montará seu grupo de heróis para proteger o rei se aventurando por terras fantásticas, lutando contra reis temíveis e exércitos mortais. Use seu tesouro estrategicamente para contratar e fortalecer heróis poderosos... Ou o Bardo.";
+    if (element[0])
+      element[0].innerHTML = "Nesse jogo você montará seu grupo de heróis para proteger o rei se aventurando por terras fantásticas, lutando contra reis temíveis e exércitos mortais. Use seu tesouro estrategicamente para contratar e fortalecer heróis poderosos... Ou o Bardo.";
     var element = document.getElementsByClassName("VejaOJogoNaSteam");
-    element[0].innerHTML = "Veja o jogo na Steam!";
+    if (element[0])
+      element[0].innerHTML = "Veja o jogo na Steam!";
   }
 }
